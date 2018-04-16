92076593a6d447f398c37e0fb1d9f497cc69bf3b
MARIO NISHIO <mario.nishio@gmail.com>
2018-04-16 15:36:22 -0700

    added in a feature where no two image out of the three is the same

--- js/app.js
+++ js/app.js
@@ -10,12 +10,19 @@ var voteButton1 = document.getElementById('vote-button-1');
 var voteButton2 = document.getElementById('vote-button-2');
 var voteButton3 = document.getElementById('vote-button-3');
 
+var voteCounter = 0;
+
+var mainEl = document.getElementById('mainElement');
+var sectionEl = document.createElement('section');
+sectionEl.id = 'resultSection';
+
 function Merch(name, url, id) {
   this.name = name;
   this.url = url;
   this.id = id;
   this.voted = 0;
   this.appeared = 0;
+  this.voteRatio = 0;
 }
 
 var allMerch = [
@@ -24,7 +31,7 @@ var allMerch = [
   new Merch('Restroom iPad Holder', 'img/bathroom.jpg', 'bathroom'),
   new Merch('Sandle Boots', 'img/boots.jpg', 'boot'),
   new Merch('Breakfast Machine', 'img/breakfast.jpg', 'breakfast'),
-  new Merch('Meatball Gum', 'img/bubblegum.', 'bubblegum'),
+  new Merch('Meatball Gum', 'img/bubblegum.jpg', 'bubblegum'),
   new Merch('Bump Chair', 'img/chair.jpg', 'chair'),
   new Merch('Action-hulhu', 'img/cthulhu.jpg', 'cthulhu'),
   new Merch('Buack!', 'img/dog-duck.jpg', 'dog-duck'),
@@ -33,10 +40,10 @@ var allMerch = [
   new Merch('Pet-cuum', 'img/pet-sweep.jpg', 'pet-sweep'),
   new Merch('Pizza Scissors', 'img/scissors.jpg', 'scissors'),
   new Merch('Rest in Shark', 'img/shark.jpg', 'shark'),
-  new Merch('Infant-cuum', 'img/sweep.jpg', 'sweep'),
+  new Merch('Infant-cuum', 'img/sweep.png', 'sweep'),
   new Merch('Tauntaun', 'img/tauntaun.jpg', 'tauntaun'),
   new Merch('Unicorn Meat', 'img/unicorn.jpg', 'unicorn'),
-  new Merch('OctopUSB', 'img/usb.jpg', 'usb'),
+  new Merch('OctopUSB', 'img/usb.gif', 'usb'),
   new Merch('Penrose Can', 'img/water-can.jpg', 'water-can'),
   new Merch('Wine Pod', 'img/wine-glass.jpg', 'wine-glass'),
 ];
@@ -46,15 +53,43 @@ function pickNewMerch() {
   merch1.appeared++;
   merch1Img.src = merch1.url;
   // merch1Img.id = merch1Img.id;
-  merch2 = allMerch[Math.floor(Math.random() * allMerch.length )];
+  do {
+    merch2 = allMerch[Math.floor(Math.random() * allMerch.length )];
+  } while (merch1 === merch2);
   merch2.appeared++;
   merch2Img.src = merch2.url;
-  merch3 = allMerch[Math.floor(Math.random() * allMerch.length )];
+  do {
+    merch3 = allMerch[Math.floor(Math.random() * allMerch.length )];
+  } while (merch3 === merch1 || merch3 === merch2);
   merch3.appeared++;
   merch3Img.src = merch3.url;
 }
 pickNewMerch();
 
+// function createResults() {
+//   if (voteCounter >= 25) {
+//     for(var i = 0; i > allMerch.length; i++) {
+//       var pEl = document.createElement('p');
+      
+//     }  
+//   }
+// }
+
 voteButton1.addEventListener('click', function(e) {
-  merch1.
+  merch1.voted++;
+  pickNewMerch();
+  voteCounter++;
 });
+
+voteButton2.addEventListener('click', function(e) {
+  merch2.voted++;
+  pickNewMerch();
+  voteCounter++;
+});
+
+voteButton3.addEventListener('click', function(e) {
+  merch3.voted++;
+  pickNewMerch();
+  voteCounter++;
+});
+
